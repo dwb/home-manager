597ba64d0d1ca0b180a91f124d06f153cd241ca2
Joaquín Triñanes <hi@joaquint.io>
2025-07-17 21:15:13 +0200

    nushell: fix `get -i` deprecation (#7490)
    Since https://github.com/nushell/nushell/pull/16007, the recommended
    flag to avoid erroring on missing fields is `--optional`. To avoid
    compatibility issues, the builtin optional access syntax is used
    instead, which is backwards-compatible.

--- modules/programs/direnv.nix
+++ modules/programs/direnv.nix
@@ -193,9 +193,9 @@ in
                           }
                         }
                         | merge ($env.ENV_CONVERSIONS? | default {})
-                        | get -i ([[value, optional, insensitive]; [$key, false, true]] | into cell-path)
-                        | get -i from_string
-                        | default {|x| $x}
+                        | get ([[value, optional, insensitive]; [$key, true, true]] | into cell-path)
+                        | get from_string?
+                        | if ($in | is-empty) { {|x| $x} } else { $in }
                     ) $value
                     return [ $key $value ]
                 }
